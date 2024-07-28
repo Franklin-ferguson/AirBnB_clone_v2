78f8c5cf6db79d1a9d5bd99723b6491e1fecf074
Franklin-ferguson <hayiborfranklin@gmail.com>
2024-07-28 08:41:07 -0700

    h

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['100.26.227.228', '100.25.141.185']
 
 
 def do_clean(number=0):
